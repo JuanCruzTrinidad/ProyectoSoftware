d4dfa77e42194a0eda18f06e589fb7ccea116f22
Tomas Silvestre <tomas.silvestre9@gmail.com>
2020-09-27 00:35:06 -0300

    avance visual catalogo y tiles

--- Grupo-4-PS-2020/API/ClientApp/src/components/ui/catalogue/Catalogue.jsx
+++ Grupo-4-PS-2020/API/ClientApp/src/components/ui/catalogue/Catalogue.jsx
@@ -1,92 +1,48 @@
+import {
+	ButtonBase,
+	Container,
+	Grid,
+	Paper,
+	Typography,
+} from "@material-ui/core";
+
 import React from "react";
 import Tile from "./Tile";
-import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import "./catalogue.css";
+import Sidebar from "./Sidebar";
 
-const drawerWidth = 240;
+const Catalogue = () => {
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: 'auto',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
-
-export default function ClippedDrawer() {
-  const classes = useStyles();
-
-  return (
-		<div className="container">
-    <div className={classes.root}>
-      {/* <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <Toolbar />
-        <div className={classes.drawerContainer}>
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-      </Drawer> */}
-			</div>
-      <main className={classes.content}>
-        <div className="row">
-          <div className="col-md-8">
+	return (
+		<Container maxWidth={"md"}>
+			<Grid container spacing={2}>
+				<Grid item xs={3}>
+					<Sidebar />
+				</Grid>
+				<Grid item xs={9}>
           <Tile />
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+				</Grid>
+			</Grid>
+		</Container>
+	);
+};
+
+{
+	/* <div className="container">
+<div className="row">
+  <Container maxwidht="md">
+    <Grid container xs={"md"}>
+      <Grid item xs={2}>
+        <Card />
+        <CardContent />
+      </Grid>
+    </Grid>
+  </Container>
+  <div className="col-md-8">
+    <Tile />
+  </div>
+</div>
+</div> */
 }
 
-
-// const Catalogue = () => {
-// 	return (
-// 		<div className="container">
-// 			<div className="navbar float-left h-100 bg-dark">
-// 				navvar
-// 			</div>
-// 			<div className="col-md-6 mt-4">
-// 				<Tile />
-// 				<Tile />
-// 				<Tile />
-// 				<Tile />
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default Catalogue;
+export default Catalogue;
--- Grupo-4-PS-2020/API/ClientApp/src/components/ui/catalogue/Tile.jsx
+++ Grupo-4-PS-2020/API/ClientApp/src/components/ui/catalogue/Tile.jsx
@@ -1,22 +1,90 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+	ButtonBase,
+	Container,
+	Grid,
+	Paper,
+	Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		padding: theme.spacing(2),
+		marginTop: 100,
+		marginLeft: 20,
+		maxWidth: 500,
+	},
+	image: {
+		width: 128,
+		height: 128,
+	},
+	img: {
+		margin: "auto",
+		display: "block",
+		maxWidth: "100%",
+		maxHeight: "100%",
+	},
+}));
+
 
 const Tile = () => {
+
+	const classes = useStyles();
+
 	return (
-			<div className="card mb-1">
-				<div className="row">
-					<div className="col-md-3 ml-3">
+		<Paper className={classes.paper}>
+			<Grid container spacing={2}>
+				<Grid item>
+					<ButtonBase className={classes.image}>
 						<img
-							src="https://dexter.vteximg.com.br/arquivos/ids/485866-1000-1000/ADF34384_1.jpg?v=637018356491800000"
-							className="img-fluid align-content-center"
+							className={classes.img}
+							alt="complex"
+							src="https://material-ui.com/static/images/grid/complex.jpg"
 						/>
-					</div>
-					<div className="col-md-8 p-4">
-						<h4 className="card-title">Titulardo super ultra super titulardo</h4>
-						<p className="card-text"><del>Precio oferta</del></p>
-						<p className="card-text">Precio</p>						
-					</div>
-			</div>
-			</div>
+					</ButtonBase>
+				</Grid>
+				<Grid item xs={12} sm container>
+					<Grid item xs container direction="column" spacing={2}>
+						<Grid item xs>
+							<Typography gutterBottom variant="subtitle1">
+								Standard license
+							</Typography>
+							<Typography variant="body2" gutterBottom>
+								Full resolution 1920x1080 • JPEG
+							</Typography>
+							<Typography variant="body2" color="textSecondary">
+								ID: 1030114
+							</Typography>
+						</Grid>
+					</Grid>
+					<Grid item>
+						<Typography variant="subtitle1">$19.00</Typography>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Paper>
+
+		// <div className="card mb-1">
+		// 	<div className="row">
+		// 		<div className="col-md-3 ml-3">
+		// 			<img
+		// 				src="https://dexter.vteximg.com.br/arquivos/ids/485866-1000-1000/ADF34384_1.jpg?v=637018356491800000"
+		// 				className="img-fluid align-content-center"
+		// 			/>
+		// 		</div>
+		// 		<div className="col-md-8 p-4">
+		// 			<h4 className="card-title">Titulardo super ultra super titulardo</h4>
+		// 			<p className="card-text">
+		// 				<del>Precio oferta</del>
+		// 			</p>
+		// 			<p className="card-text">Precio</p>
+		// 		</div>
+		// 	</div>
+		// </div>
 	);
 };
 
