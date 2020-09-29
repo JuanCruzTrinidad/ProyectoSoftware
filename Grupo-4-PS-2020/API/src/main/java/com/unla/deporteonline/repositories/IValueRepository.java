727faffa7300349112cb6f5b8f09bf0ee4fa3214
fiorda04 <51273571+fiorda04@users.noreply.github.com>
2020-09-28 22:37:58 -0300

    alta valoracion

---
+++ Grupo-4-PS-2020/API/src/main/java/com/unla/deporteonline/repositories/IValueRepository.java
@@ -0,0 +1,16 @@
+package com.unla.deporteonline.repositories;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.unla.deporteonline.entities.*;
+
+    
+@Repository("ValueRepository") 
+public interface IValueRepository extends JpaRepository<Valoracion, Serializable> {
+
+}
---
+++ Grupo-4-PS-2020/API/src/main/java/com/unla/deporteonline/services/IValueService.java
@@ -0,0 +1,12 @@
+package com.unla.deporteonline.services;
+
+import com.unla.deporteonline.entities.Valoracion;
+import java.util.List;
+import java.util.Optional;
+
+
+public interface IValueService {
+    
+    public Object saveValue(Valoracion value);
+   
+}
