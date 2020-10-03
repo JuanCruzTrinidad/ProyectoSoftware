2ddba8588d1b0a95493f3fefb2f403cf4e53874b
Tomas Silvestre <tomas.silvestre9@gmail.com>
2020-10-02 22:30:33 -0300

    test datos y modificacion sidebar en catalogo

--- Grupo-4-PS-2020/API/ClientApp/src/components/ui/catalogue/Catalogue.jsx
+++ Grupo-4-PS-2020/API/ClientApp/src/components/ui/catalogue/Catalogue.jsx
@@ -5,17 +5,31 @@ import {
   Paper,
   Typography,
 } from "@material-ui/core";
-
-import React, {Fragment, useState} from "react";
+import React, { Fragment, useState } from "react";
 import Tile from "./Tile";
 import "./catalogue.css";
 import Sidebar from "./Sidebar";
 import MediaCard from "./Card";
 import Search from "./Search";
+import PaginationOutlined from "./PaginationOutlined";
 
 const Catalogue = () => {
+  const [visual, setvisual] = useState("tiles");
 
-  const [visual, setvisual] = useState('cards');
+  const listaproductos = [
+    { id: 1, name: "Botines1" },
+    { id: 2, name: "Botines2" },
+    { id: 3, name: "Botines3" },
+    { id: 4, name: "Botines4" },
+    { id: 5, name: "Botines5" },
+  ];
+
+  const listacategorias = [
+    {id: 1, name: "Categoria1" },
+    {id: 2, name: "Categoria2" },
+    {id: 3, name: "Categoria3" },
+    {id: 4, name: "Categoria4" }
+  ]
 
   return (
     <div className="contenedor">
@@ -23,54 +37,36 @@ const Catalogue = () => {
         <Grid container spacing={1}>
           <Grid item xs={3}>
             <Search />
-            <Sidebar
-              visual={visual}
-              setvisual={setvisual}
+            <Sidebar 
+              visual={visual} 
+              setvisual={setvisual} 
+              listacategorias={listacategorias}
             />
           </Grid>
-          {visual === "tiles" ?
+          {visual === "tiles" ? (
             <Grid item xs={9}>
-              <Tile />
-              <Tile />
-              <Tile />
-            </Grid> 
-          :
-            <Fragment >
-              <Grid item xs={9}>
-            <div class="card-group">
-              <MediaCard />
-              <MediaCard />
-              <MediaCard />
-              <MediaCard />
-              <MediaCard />
-              <MediaCard />
-              <MediaCard />
-            </div>
+              {listaproductos.map((prod, index) => (
+                <Tile key={prod.id} prod={prod} />
+              ))}
             </Grid>
+          ) : (
+            <Fragment>
+              <Grid item xs={9}>
+                <div class="card-group">
+                  {listaproductos.map((prod, index) => (
+                    <MediaCard key={prod.id} prod={prod} />
+                  ))}
+                </div>
+              </Grid>
             </Fragment>
-          }
+          )}
+        </Grid>
+        <Grid container>
+          <PaginationOutlined />
         </Grid>
       </Container>
     </div>
   );
 };
-
-{
-  /* <div className="container">
-<div className="row">
-  <Container maxwidht="md">
-    <Grid container xs={"md"}>
-      <Grid item xs={2}>
-        <Card />
-        <CardContent />
-      </Grid>
-    </Grid>
-  </Container>
-  <div className="col-md-8">
-    <Tile />
-  </div>
-</div>
-</div> */
-}
 
 export default Catalogue;
